310773a7aa6434ae69b9de911f0df3fc3ab846e7
had2020 <hadrian.lazic@gmail.com>
2026-07-22 14:37:28 -0700

    upd

--- cpu-accelerated/src/lib.rs
+++ cpu-accelerated/src/lib.rs
@@ -1,31 +1,22 @@
 use drakon_opcodes::*;
 
-const MAX_INSTRUCTIONS: usize = 8;
-
 /// Cache lane 64 byte aligned layout:
-/// VM_0[Inst*4Bytes: u32, Inst*4Bytes: u32]
-/// VM_1[Inst*4Bytes: u32, Inst*4Bytes: u32]
-/// VM_2[Inst*4Bytes: u32, Inst*4Bytes: u32]
-/// VM_3[Inst*4Bytes: u32, Inst*4Bytes: u32]
 #[repr(C, align(64))]
 pub struct OpcodeLane {
-    pub genomes: [u32; 16],
+    pub vm_genes: [[u8; 8]; 8], // 8 VMs each with 8-8bit word max instructions.
 }
 
 // This ticks entire OpcodeLane at once, with optimized ALU port usage.
 #[inline(always)]
-pub fn tick_lane(lane: OpcodeLane) -> [[u32; 4]; 4] {
-    let mut vm_regs_0: [u32; 4] = [0, 0, 0, 0];
-    let mut vm_regs_1: [u32; 4] = [0, 0, 0, 0];
-    let mut vm_regs_2: [u32; 4] = [0, 0, 0, 0];
-    let mut vm_regs_3: [u32; 4] = [0, 0, 0, 0];
+pub fn tick_lane(lane: &OpcodeLane, register_preload_input: &[u32; 4]) -> [[u32; 4]; 4] {
+    // 16 Registers used
+    let mut vm_regs_bank: [[u32; 4]; 4] = [register_preload_input.clone(); 4];
 
-    for i in 0..MAX_INSTRUCTIONS {
-        // Should be unrolled by compiler!
-        for j in 0..3 {
-            lane.vm_0
+    for i in 0..8 {
+        for j in 0..8 {
+            let instruction = lane.vm_genes[i][j];
         }
     }
 
-    [vm_regs_0, vm_regs_1, vm_regs_2, vm_regs_3] // Cache Aligned as well!
+    vm_regs_bank
 }
