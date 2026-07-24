f3943b664240838a2e7282f42376434a5b0a2243
had2020 <hadrian.lazic@gmail.com>
2026-07-24 00:22:06 -0700

    updates

--- fitness-parameters/src/lib.rs
+++ fitness-parameters/src/lib.rs
@@ -1,1 +1,6 @@
-pub struct task_dataset
+pub const MAX_SEARCH_SPACE: u32 = 16777216;
+
+pub struct TestDataset<const TEST_SETS: usize> {
+    pub register_inputs: [[u32; 4]; TEST_SETS],
+    pub expected_outputs: [[u32; 4]; TEST_SETS],
+}
