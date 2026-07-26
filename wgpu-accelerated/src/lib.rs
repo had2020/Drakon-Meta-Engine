4625b4aecf54189cfe058a87f6e257f15cca1880
had2020 <hadrian.lazic@gmail.com>
2026-07-26 15:26:20 -0700

    Progresss on high level wrapper to wgpu shader, for quick mutation searches.

--- wgpu-accelerated/src/lib.rs
+++ wgpu-accelerated/src/lib.rs
@@ -6,7 +6,7 @@ use wgpu::util::DeviceExt;
 pub struct WgpuWrapper {
     device: wgpu::Device,
     queue: wgpu::Queue,
-    shader_module: wgpu::ShaderModule,
+    module: wgpu::ShaderModule,
 }
 
 impl WgpuWrapper {
@@ -40,20 +40,120 @@ impl WgpuWrapper {
         Self {
             device: device,
             queue: queue,
-            shader_module: module,
+            module: module,
         }
     }
 }
 
-pub fn gpu_raw_search(wgpu_wrapper: WgpuWrapper, training_dataset: TrainingDataset) -> [u8; 8] {
+pub fn gpu_raw_search<const TEST_SETS: usize>(
+    wgpu_wrapper: &WgpuWrapper,
+    training_dataset: &TrainingDataset<TEST_SETS>,
+) -> [u8; 8] {
+    let dataset_bytes = training_dataset.to_bytes_form();
+
     let input_data_buffer =
         wgpu_wrapper
             .device
             .create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                label: None,
-                contents: training_dataset.to_bytes().as_slice(),
+                label: Some("Training Dataset Buffer"),
+                contents: dataset_bytes.as_slice(),
                 usage: wgpu::BufferUsages::STORAGE,
             });
 
-    [0_u8; 8]
+    let output_data_buffer = wgpu_wrapper.device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size: input_data_buffer.size(),
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
+    });
+
+    let download_buffer = wgpu_wrapper.device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size: input_data_buffer.size(),
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+        mapped_at_creation: false,
+    });
+
+    let bind_group_layout =
+        wgpu_wrapper
+            .device
+            .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: None,
+                entries: &[
+                    // input buffer
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: true },
+                            // the size of a single element in the buffer
+                            min_binding_size: Some(NonZeroU64::new(4).unwrap()),
+                            has_dynamic_offset: false,
+                        },
+                        count: None,
+                    },
+                    // output buffer
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::COMPUTE,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Storage { read_only: false },
+                            min_binding_size: Some(NonZeroU64::new(4).unwrap()),
+                            has_dynamic_offset: false,
+                        },
+                        count: None,
+                    },
+                ],
+            });
+
+    let bind_group = wgpu_wrapper
+        .device
+        .create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: input_data_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: output_data_buffer.as_entire_binding(),
+                },
+            ],
+        });
+
+    let pipeline_layout =
+        wgpu_wrapper
+            .device
+            .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[Some(&bind_group_layout)],
+                immediate_size: 0,
+            });
+
+    let pipeline = wgpu_wrapper
+        .device
+        .create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            module: &wgpu_wrapper.module,
+            entry_point: Some("MetaDrakon"),
+            compilation_options: wgpu::PipelineCompilationOptions::default(),
+            cache: None,
+        });
+
+    let mut encoder = wgpu_wrapper
+        .device
+        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+    let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+        label: None,
+        timestamp_writes: None,
+    });
+
+    compute_pass.set_pipeline(&pipeline);
+    compute_pass.set_bind_group(0, &bind_group, &[]);
+
+    [0_u8; 8] // <- TODO:
 }
